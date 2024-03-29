c095f086b8fc47316914595209bb3119ae639132
Jordan Day <jordaeday22@gmail.com>
2024-03-29 13:38:06 -0400

    created navbar script

---
+++ src/docs/milestone-01/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav a");
+    //console.log(navLinks);
+    const sections = document.querySelectorAll("main section");
+    //console.log(sections);
+
+    function hideAll() {
+        sections.forEach(section => {
+            section.style.display = "none";
+        });
+    }
+    hideAll();
+
+    function showSection(sectionId) {
+        console.log(sectionId);
+        const section = document.getElementById(sectionId);
+        console.log(section);
+        console.log(section.style);
+        console.log(section.style.display);
+        section.style.display = "block";
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const sectionId = link.getAttribute("href").substring(1); // removes the # from the href
+            hideAll();
+            showSection(sectionId);
+        });
+    });
+});
