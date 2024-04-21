fd88cf4ef3acd78e8566178664128cd3a650b00a
Manya Phutela <mphutela@umass.edu>
2024-04-21 18:03:25 -0400

    added mock locations

--- src/client/app.js
+++ src/client/app.js
@@ -1,12 +1,60 @@
 function showPage(pageId) {
-    // Hide all pages
-    document.querySelectorAll('.page').forEach(page => {
-        page.style.display = 'none';
-    });
+  // Hide all pages
+  document.querySelectorAll(".page").forEach((page) => {
+    page.style.display = "none";
+  });
 
-    // Show the requested page
-    document.getElementById(pageId).style.display = 'block';
+  // Show the requested page
+  document.getElementById(pageId).style.display = "block";
 }
 
 // Initial display setup
-showPage('home'); // Show home page by default
+showPage("home"); // Show home page by default
+
+const locations = [
+  {
+    name: "Tokyo",
+    about: {
+      budget: "whatever",
+      weather: "weather",
+      tags: ["city", "unique", "electronics"],
+      language: ["Japanese"],
+    },
+  },
+  {
+    name: "Paris",
+    about: {
+      budget: "whatever",
+      weather: "weather",
+      tags: ["romantic", "cultural", "art"],
+      language: ["French"],
+    },
+  },
+  {
+    name: "Bora Bora",
+    about: {
+      budget: "whatever",
+      weather: "weather",
+      tags: ["beach", "relaxation", "tropical"],
+      language: ["English", "French"],
+    },
+  },
+  {
+    name: "Rome",
+    about: {
+      budget: "whatever",
+      weather: "weather",
+      tags: ["cultural", "art", "city"],
+      language: ["Italian"],
+    },
+  },
+  {
+    name: "Los Angeles",
+    about: {
+      budget: "whatever",
+      weather: "weather",
+      tags: ["tropical", "city", "cultural"],
+      language: ["English"],
+    },
+  },
+];
